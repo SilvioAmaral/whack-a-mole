5ca45fa2ae3cf8694a2ceea33d5c6f2183eb3c6a
Silvio <silvio.amaral@gmail.com>
2020-04-09 15:21:16 -0400

    Whack-a-mole game

---
+++ app.js
@@ -0,0 +1,44 @@
+const squares = document.querySelectorAll(".square");
+const mole = document.querySelectorAll(".mole");
+const timeLeft = document.querySelector("#time-left");
+
+let score = document.querySelector("#score");
+let result = 0;
+let currentTime = timeLeft.textContent;
+let timerId;
+
+squares.forEach((square) => {
+  square.addEventListener("mouseup", () => {
+    if (square.id === hitPosition) {
+      result++;
+      score.textContent = result;
+    }
+  });
+});
+
+function randomSquare() {
+  squares.forEach((square) => {
+    square.classList.remove("mole");
+  });
+  let randomPosition = squares[Math.floor(Math.random() * 9)];
+  randomPosition.classList.add("mole");
+
+  hitPosition = randomPosition.id;
+}
+
+function moveMole() {
+  timerId = null;
+  timerId = setInterval(randomSquare, 1000);
+}
+
+function countDown() {
+  currentTime--;
+  timeLeft.textContent = currentTime;
+  if (currentTime === 0) {
+    clearInterval(timerId);
+    alert(`Game over. Result: ${result}`);
+  }
+}
+
+moveMole();
+timerId = setInterval(countDown, 1000);
